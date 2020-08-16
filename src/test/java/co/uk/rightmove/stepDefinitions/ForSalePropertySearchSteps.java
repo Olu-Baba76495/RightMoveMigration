ea7281497d7145ae1a3a7a8e45988024ac747f55
olu-baba76495 <olu76495@yahoo.com>
2020-08-16 17:44:54 +0100

    BABA-3 - Adding new feature file and step definition

---
+++ src/test/java/co/uk/rightmove/stepDefinitions/ForSalePropertySearchSteps.java
@@ -0,0 +1,80 @@
+package co.uk.rightmove.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ForSalePropertySearchSteps {
+
+    @Given("I navigate to rightmove homepage")
+    public void i_navigate_to_rightmove_homepage() {
+
+    }
+
+    @When("I enter {string} into the search field")
+    public void i_enter_into_the_search_field(String string) {
+
+    }
+
+    @When("I click on For sale button")
+    public void i_click_on_for_sale_button() {
+
+    }
+
+    @When("I select {string} from the Search radius dropdown")
+    public void i_select_from_the_search_radius_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from the Min Price range dropdown")
+    public void i_select_from_the_min_price_range_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from the Max Price range dropdown")
+    public void i_select_from_the_max_price_range_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from the Min No. of bedrooms dropdown")
+    public void i_select_from_the_min_no_of_bedrooms_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from the Max No. of bedrooms dropdown")
+    public void i_select_from_the_max_no_of_bedrooms_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from Property type dropdown")
+    public void i_select_from_property_type_dropdown(String string) {
+
+    }
+
+    @When("I select  {string} from Added to site dropdown")
+    public void i_select_from_added_to_site_dropdown(String string) {
+
+    }
+
+    @When("I click on Find properties button")
+    public void i_click_on_find_properties_button() {
+
+    }
+
+    @Then("search result page is displayed with relevant results")
+    public void search_result_page_is_displayed_with_relevant_results() {
+
+    }
+
+    @Then("a popup window is displayed to enter area,postcode or train station")
+    public void a_popup_window_is_displayed_to_enter_area_postcode_or_train_station() {
+
+    }
+
+    @Then("an error page is displayed")
+    public void an_error_page_is_displayed() {
+
+    }
+
+
+}
